e0130e3a7f0e1518841539fe9dc1379a162bdb76
Ramazan Arıcı <ramazanarc5@gmail.com>
2024-02-28 10:11:02 +0300

    Google Auth

--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,28 +1,30 @@
 import { Avatar, Button, Flex, Text } from "@chakra-ui/react";
-import { Link, Link as RouterLink } from "react-router-dom";
 import useLogout from "../../hooks/useLogout";
 import useAuthStore from "../../store/authStore";
+import { Link } from "react-router-dom";
 
 const SuggestedHeader = () => {
   const { handleLogout, isLoggingOut } = useLogout();
-  const authuser = useAuthStore((state) => state.user);
+  const authUser = useAuthStore((state) => state.user);
+
+  if (!authUser) return null;
+
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
       <Flex alignItems={"center"} gap={2}>
-        <Link to={`${authuser.username}`}>
-          <Avatar size={"lg"} src={authuser.profilePicURL} />
+        <Link to={`${authUser.username}`}>
+          <Avatar size={"lg"} src={authUser.profilePicURL} />
         </Link>
-
-        <Link to={`${authuser.username}`}>
+        <Link to={`${authUser.username}`}>
           <Text fontSize={12} fontWeight={"bold"}>
-            {authuser.username}
+            {authUser.username}
           </Text>
         </Link>
       </Flex>
       <Button
         size={"xs"}
         background={"transparent"}
-        _hover={{ background: "trasnparent" }}
+        _hover={{ background: "transparent" }}
         fontSize={14}
         fontWeight={"medium"}
         color={"blue.400"}
