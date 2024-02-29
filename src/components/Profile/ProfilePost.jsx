47fa5848d33f143e29d3c95cdbe8a768fb73d5f7
Ramazan Arıcı <ramazanarc5@gmail.com>
2024-02-29 11:19:09 +0300

    Delete Post

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -14,15 +14,51 @@ import {
   Avatar,
   Divider,
   VStack,
+  Button,
 } from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import Comment from "../Comment/Comment";
 import PostFooter from "../FeedPosts/PostFooter";
+import useUserProfileStore from "../../store/userProfileStore";
+import useAuthStore from "../../store/authStore";
+import useShowToast from "../../hooks/useShowToast";
+import { useState } from "react";
+import { deleteObject, ref } from "firebase/storage";
+import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { firestore, storage } from "../../firebase/firebase";
+import usePostStore from "../../store/postStore";
 
-const ProfilePost = ({ img }) => {
+const ProfilePost = ({ post }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const userProfile = useUserProfileStore((state) => state.userProfile);
+  const authUser = useAuthStore((state) => state.user);
+  const showToast = useShowToast();
+  const [isDeleting, setIsDeleting] = useState(false);
+  const deletePost = usePostStore((state) => state.deletePost);
+  const decrementPostsCount = useUserProfileStore((state) => state.deletePost);
+  const handleDeletePost = async () => {
+    if (!window.confirm("Are you sure you want to delete this post ?")) return;
+    if (isDeleting) return;
+    try {
+      const imageRef = ref(storage, `posts/${post.id}`);
+      await deleteObject(imageRef);
+      const userRef = doc(firestore, "users", authUser.uid);
+      await deleteDoc(doc(firestore, "posts", post.id));
+      await updateDoc(userRef, {
+        posts: arrayRemove(post.id),
+      });
+
+      deletePost(post.id);
+      decrementPostsCount(post.id);
+      showToast("Success", "Post deleted succesfully", "success");
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsDeleting(false);
+    }
+  };
   return (
     <>
       <GridItem
@@ -52,20 +88,20 @@ const ProfilePost = ({ img }) => {
             <Flex>
               <AiFillHeart size={20} />
               <Text fontWeight={"bold"} ml={2}>
-                7
+                {post.likes.length}
               </Text>
             </Flex>
             <Flex>
               <FaComment size={20} />
               <Text fontWeight={"bold"} ml={2}>
-                7
+                {post.comments.length}
               </Text>
             </Flex>
           </Flex>
         </Flex>
 
         <Image
-          src={img}
+          src={post.imageURL}
           alt="profile post"
           w={"100%"}
           h={"100%"}
@@ -87,16 +123,20 @@ const ProfilePost = ({ img }) => {
               gap={4}
               w={{ base: "90%", sm: "70%", md: "full" }}
               mx={"auto"}
+              maxH={"90vh"}
+              minH={"50vh"}
             >
-              <Box
+              <Flex
                 borderRadius={4}
                 overflow={"hidden"}
                 border={"1px solid"}
                 borderColor={"whiteAlpha.300"}
                 flex={1.5}
+                justifyContent={"center"}
+                alignItems={"center"}
               >
-                <Image src={img} alt="profile post" />
-              </Box>
+                <Image src={post.imageURL} alt="profile post" />
+              </Flex>
 
               <Flex
                 flex={1}
@@ -107,22 +147,28 @@ const ProfilePost = ({ img }) => {
                 <Flex alignItems={"center"} justifyContent={"space-between"}>
                   <Flex alignItems={"center"} gap={4}>
                     <Avatar
-                      src="/profilepic.png"
+                      src={userProfile.profilePicURL}
                       size={"sm"}
                       name="As a prgrammer"
                     />
                     <Text fontWeight={"bold"} fontSize={12}>
-                      asaprogrammer_
+                      {userProfile.username}
                     </Text>
                   </Flex>
 
-                  <Box
-                    _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
-                    borderRadius={4}
-                    p={1}
-                  >
-                    <MdDelete size={20} cursor="pointer" />
-                  </Box>
+                  {authUser?.uid === userProfile.uid && (
+                    <Button
+                      _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                      borderRadius={4}
+                      p={1}
+                      size={"s"}
+                      bg={"transparent"}
+                      onClick={handleDeletePost}
+                      isLoading={isDeleting}
+                    >
+                      <MdDelete size={20} cursor="pointer" />
+                    </Button>
+                  )}
                 </Flex>
                 <Divider my={4} bg={"gray.500"} />
 
@@ -131,58 +177,7 @@ const ProfilePost = ({ img }) => {
                   alignItems={"start"}
                   maxH={"350px"}
                   overflowY={"auto"}
-                >
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-                  <Comment
-                    createdAt="1d ago"
-                    username="asaprogrammer"
-                    profilePic="/profilepic.png"
-                    text={"Dummy images from unsplash"}
-                  />
-                </VStack>
+                ></VStack>
                 <Divider my={4} bg={"gray.800"} />
 
                 <PostFooter isProfilePage={true} />
--- src/store/postStore.js
+++ src/store/postStore.js
@@ -2,7 +2,10 @@ import { create } from "zustand";
 
 const usePostStore = create((set) => ({
   posts: [],
-  createPost: (posts) => set((state) => ({ posts: posts, ...state.posts })),
+  createPost: (post) => set((state) => ({ posts: [post, ...state.posts] })),
+  setPosts: (posts) => set({ posts }),
+  deletePost: (id) =>
+    set((state) => ({ posts: state.posts.filter((post) => post.id !== id) })),
 }));
 
 export default usePostStore;
